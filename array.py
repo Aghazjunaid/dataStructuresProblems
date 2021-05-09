7423a961eb515c2b311917bed34c0b0bbb072193
unknown <aghazjunaid96@gmail.com>
2021-05-09 23:47:59 +0530

    # 7. Find the maximum and minimum element in an array

---
+++ array.py
@@ -0,0 +1,12 @@
+# 7. Find the maximum and minimum element in an array
+opt = [21,23,6,13,34,112,14,10,24,45,5,12,5,46,79,7]
+max = opt[0]
+min = opt[0]
+for i in range(1,len(opt)):
+    if opt[i]>max:
+        max=opt[i]
+    elif opt[i]<min:
+        min=opt[i]
+print(max,min)
+
+# 112 5
