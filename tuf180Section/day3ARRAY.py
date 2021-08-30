3e37485eb2c0141201a8b3fd8629e165ae5de8ff
unknown <aghazjunaid96@gmail.com>
2021-08-30 19:15:32 +0530

    day3

--- tuf180Section/day3ARRAY.py
+++ tuf180Section/day3ARRAY.py
@@ -16,3 +16,33 @@
 #         high = mid-1
     
 # # Element found at [2, 1]
+
+
+# # Majority Element
+# arr = [2,2,1,1,1,2,2]
+
+# k = len(arr)//2
+
+# for ele in arr:
+#     count = 0
+#     for i in arr:
+#         if ele == i:
+#             count+=1
+#     if count > len(arr)//2:
+#         print(ele) # 2
+
+
+# # OR Moore Voting Algorithm
+# arr = [2,2,1,1,1,2,2]
+
+# cut = 0
+# el = 0
+# for i in range(len(arr)):
+#     if cut == 0:
+#         el = arr[i]
+#     if arr[i] == el:
+#         cut += 1
+#     else:
+#         cut -= 1
+
+# print(el) # 2
