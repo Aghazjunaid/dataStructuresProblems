d358101edea7ae24415b7037a2f125f97119316c
unknown <aghazjunaid96@gmail.com>
2021-08-30 12:21:29 +0530

    day4

---
+++ tuf180Section/day4HASHING.py
@@ -0,0 +1,12 @@
+# 2 Sum Problem
+# arr = [2,6,5,18,7]
+# target = 14
+
+# for i in range(len(arr)):
+#     for j in range(i+1,len(arr)):
+#         if target - arr[i] == arr[j]:
+#             print([i,j])
+
+# # [1, 4]
+
+# OR
