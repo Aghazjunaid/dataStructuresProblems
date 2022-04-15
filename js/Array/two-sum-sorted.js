090f9c0ae6619869ab0772dc75c3e4338cf6f91d
unknown <aghazjunaid96@gmail.com>
2022-04-15 10:53:32 +0530

    two sum sorted

---
+++ js/Array/two-sum-sorted.js
@@ -0,0 +1,37 @@
+// function twoSumSOrted(arr){
+//     for(let i in arr){
+//         let num = -arr[i]
+//         for(let j=i+1;j<arr.length;j++){
+//             if(arr[j] == num){
+//                 return true
+//             }
+//         }
+//     }
+//     return false
+// }
+
+// console.log(twoSumSOrted([-3, 1, 3, 4])) //true
+// console.log(twoSumSOrted([-3, 1, 2, 4])) //false
+// console.log(twoSumSOrted([3, 1, -3, 4])) //true
+
+
+// 2(Best Method)
+function twoSumSOrted(arr){
+    let l = 0;
+    let r = arr.length-1
+    while(l<r){
+        if(arr[l]+arr[r] > 0){
+            r--
+        } else if(arr[l]+arr[r]< 0){
+            l++
+        } else if(arr[l]+arr[r] == 0){
+            console.log(arr[l],arr[r])
+            return true
+        }
+    }
+    return false
+}
+
+console.log(twoSumSOrted([-3, 1, 3, 4])) //true
+console.log(twoSumSOrted([-3, 1, 2, 4])) //false
+console.log(twoSumSOrted([-19,-3, 0, 3, 4,5,7,8])) //true
