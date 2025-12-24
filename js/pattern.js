8e1c52c75db93ce6b38489d2270517fbc0353c5d
Aghaz Junaid <64908660+Aghazjunaid@users.noreply.github.com>
2025-12-24 23:36:19 +0530

    Add various number and star pattern outputs

--- js/pattern.js
+++ js/pattern.js
@@ -37,3 +37,130 @@ for(let i=6;i>=1;i--){
 }
 
 console.log(ans)
+
+3.
+// 1 
+// 2 3 
+// 4 5 6 
+// 7 8 9 10 
+// 11 12 13 14 15 
+// 16 17 18 19 20 21
+
+let n = 6;
+let ans = '';
+
+let num = 1
+for(let i=1;i<=n;i++){
+  for(let j=0;j<i;j++){
+    ans += `${num} `
+    num++
+  }
+  ans += '\n'
+}
+
+console.log(ans)
+
+4.
+// 1 
+// 0 1 
+// 1 0 1 
+// 0 1 0 1 
+// 1 0 1 0 1 
+// 0 1 0 1 0 1
+
+let n = 6;
+let ans = '';
+
+let num = true;
+for(let i=1;i<=n;i++){
+  for(let j=1;j<=i;j++){
+    ans += `${Number(num)} `
+    num = !num
+  }
+  if(i%2 == 0) num = !num
+  ans += '\n'
+}
+
+console.log(ans)
+
+5.
+// ******
+// ******
+// ******
+// ******
+// ******
+// ******
+
+let n = 6;
+let ans = '';
+
+for(let i=1;i<=n;i++){
+  for(let j=1;j<=n;j++){
+    ans += '*'
+  }
+  ans += '\n'
+}
+
+console.log(ans)
+
+6.
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+let n = 6;
+let ans = '';
+
+for(let i=1;i<=n;i++){
+  for(let j=1;j<=i;j++){
+    ans += '*'
+  }
+  ans += '\n'
+}
+
+console.log(ans)
+
+7.
+// ******
+// *****
+// ****
+// ***
+// **
+// *
+
+let n = 6;
+let ans = '';
+
+for(let i=1;i<=n;i++){
+  for(let j=i;j<=n;j++){
+    ans += '*'
+  }
+  ans += '\n'
+}
+
+console.log(ans)
+
+8.
+//******
+// *****
+//  ****
+//   ***
+//    **
+//     *
+
+let n = 6;
+let ans = '';
+
+for(let i=0;i<n;i++){
+  for(let j=0;j<i;j++){
+    ans += ' '
+  }
+  for(let j=0;j<n-i;j++){
+    ans += '*'
+  }
+  ans += '\n'
+}
+
+console.log(ans)
