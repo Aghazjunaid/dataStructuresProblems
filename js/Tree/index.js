d9c4db3e458545039640ff3b3ac40cf05885c51a
Aghaz Junaid <64908660+Aghazjunaid@users.noreply.github.com>
2025-12-29 22:03:23 +0530

    Add BinaryTree class and traversal methods
    Implemented a BinaryTree class with inorder, preorder, postorder, and level order traversal methods. Added example usage of the tree and traversal functions.

---
+++ js/Tree/index.js
@@ -0,0 +1,80 @@
+class BinaryTree{
+  constructor(value){
+    this.val = value;
+    this.right = null;
+    this.left = null
+  }
+}
+
+const root = new BinaryTree(1)
+root.left = new BinaryTree(2)
+root.right = new BinaryTree(3)
+root.left.left = new BinaryTree(4)
+root.left.right = new BinaryTree(5)
+root.right.right = new BinaryTree(6)
+
+
+function inorder(root){
+  if(!root) return;
+  
+  inorder(root.left)
+  console.log(root.val)
+  inorder(root.right)
+}
+inorder(root)
+
+function preorder(root){
+  if(!root) return
+  
+  console.log(root.val)
+  preorder(root.left)
+  preorder(root.right)
+}
+preorder(root)
+
+function postorder(root){
+  if(!root) return
+  
+  postorder(root.left)
+  postorder(root.right)
+  console.log(root.val)
+}
+postorder(root)
+
+function levelOrderByLevel(root) {
+  if (root === null) return [];
+
+  let result = [];
+  let queue = [root];
+
+  while (queue.length > 0) {
+    let size = queue.length;
+    let level = [];
+
+    for (let i = 0; i < size; i++) {
+      let node = queue.shift();
+      level.push(node.val);
+
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+
+    result.push(level);
+  }
+
+  return result;
+}
+
+console.log(levelOrderByLevel(root))
+BFS ka Idea (Simple Soch)
+1. Root ko queue me daalo
+2. Jab tak queue empty na ho:
+    Front node nikalo
+    Print karo
+    Uske left child ko queue me daalo (agar exist karta ho)
+    Uske right child ko queue me daalo (agar exist karta ho)
+
+Q1. Maximum Depth or Height of a Binary Tree
+
+
+
