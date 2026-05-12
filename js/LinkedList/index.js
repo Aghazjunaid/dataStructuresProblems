ecc2c322d2d4a9ab462f87ac408bb12778dfb26b
Aghaz Junaid <64908660+Aghazjunaid@users.noreply.github.com>
2026-05-12 19:46:32 +0530

    Add prepend and insertAt methods to LinkedList

--- js/LinkedList/index.js
+++ js/LinkedList/index.js
@@ -1,3 +1,7 @@
+//Note:-
+// 1. while(current !== null) - jab tak current node exist karta hai
+// 2. while(current.next !== null) - jab tak next node exist karta hai
+
 class Node{
   constructor(data){
     this.data = data;
@@ -28,10 +32,48 @@ class LinkedList{
     
     current.next = node
   }
-
+  
   // =========================
-  // PRINT LIST
+  // INSERT AT BEGINNING
   // =========================
+  prepend(data){
+    let node = new Node(data);
+    
+    if(this.head == null){
+      this.head = node;
+      return
+    } 
+    
+    node.next = this.head
+    this.head = node;
+  }
+  
+   // =========================
+  // INSERT AT SPECIFIC INDEX
+  // =========================
+  insertAt(data,index){
+    let node = new Node(data)
+    
+    if(index == 0){
+      node.next = this.head;
+      this.head = node;
+      return;
+    }
+    
+    let current = this.head;
+    let prev = null;
+    let count = 0;
+    
+    while(current != null && count < index){
+      prev = current;
+      current = current.next;
+      count++
+    }
+    
+    prev.next = node;
+    node.next = current
+  }
+  
   print(){
     
     let current = this.head;
@@ -48,4 +90,6 @@ let list = new LinkedList();
 list.append(10);
 list.append(20);
 list.append(30);
+list.prepend(1)
+list.insertAt(25,3)
 list.print()
